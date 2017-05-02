94127458aba2adc8f2901cd23319cab077c7e949
Michael Chang <mbchang@mit.edu>
2017-05-02 19:28:16 -0400

    these dependencies work for ps3 arithmetic

--- code/load.scm
+++ code/load.scm
@@ -1,4 +1,7 @@
-(load (list "utils"                     ;from common
+(load (list 
+
+
+            "utils"                     ;from common
             "indexes"                   ;from common
 	    "collections"               ;from common
             "memoizers"                 ;from common
@@ -10,21 +13,100 @@
 	    "package"                   ;from common
 	    "predicate-counter"         ;from common
             "trie"                      ;from common
-            "generics"
-            "tagging"
-	    "predicates1"
-            "templates"
-            "values"
-            "tags"
-            "functions"
-	    "operations"                
-	    "arith"			;from common
-	    "numeric-arith"		;from common
-            "vector-extender"
-            "generic-arith"             ;from generic-arithmetic
-	    "standard-arith"            
+     ;       "generics"
+            ;"tagging"
+	    ;"predicates1"
+     ;       "templates"
+     ;       "values"
+     ;       "tags"
+     ;       "functions"
+
+
+
+
+	    ;"operations"                
+	    ;"arith"			;from common
+	    ;"numeric-arith"		;from common
+     ;       ;"vector-extender"
+     ;       "generic-arith"             ;from generic-arithmetic
+	    ;"standard-arith"            
+
+
+            ;"p3-utils"                     ;from common
+          ;"p3-collections"               ;from common
+            ;"p3-memoizers"                 ;from common
+          ;"p3-predicates"                ;from common
+          "p3-predicate-metadata"        ;from common
+          ;"p3-applicability"             ;from common
+            ;"p3-generic-procedures"        ;from common
+          ;"p3-operators"                 ;from common
+          "p3-operations"                ;from common
+          ;"p3-package"                   ;from common
+          ;"p3-predicate-counter"         ;from common
+            ;"p3-trie"                      ;from common
+
+          "arith"             ;from common
+          "numeric-arith"           ;from common
+            
+          ;"p3-arith"             ;from common
+          ;"p3-numeric-arith"           ;from common
+
+
+          "p3-standard-arith"            ;from arithmetic-combinators
+          "p3-function-variants"         ;from arithmetic-combinators
+            "p3-generic-arith"             ;from generic-arithmetic
+            "p3-generics"                  ;from generic-arithmetic
+
+
+
+          ;"p3-generics"
+          ;"p3-operations"
+          ;"arith"
+          ;"numeric-arith"
+          ;"generic-arith"
+          ;"p3-standard-arith"      ; problem
+          "p3-vector-arith"        ; problem
+
+
+
+
+
+
+
+
             "physics-substrate"
             ; "physics-objects"
             ; "physics-world"
 	    ))
+
+
+; from ps3
+; operations, arith, numeric-arith, vector-extender, generic-arith, standard-arith
+
+; arith is fine to keep, numeric-arith is fine to keep, generic-arith is fine to keep, operators is fine to keep.
+; replace standard-arith, replace vector-arith, replace operations
+
+
+;(define trie-full-generic-arithmetic-vector
+;    (let ((g (make-generic-arithmetic trie-generic-dispatcher)))
+;            (add-to-generic-arithmetic! g numeric-arithmetic)
+;            (extend-generic-arithmetic! g function-extender)
+;            (extend-generic-arithmetic! g vector-extender)
+;            (add-to-generic-arithmetic! g (symbolic-extender numeric-arithmetic))
+;    g))
+
+;;(define trie-full-generic-arithmetic-vector
+;;    (let ((g (make-generic-arithmetic trie-generic-dispatcher)))
+;;            (add-to-generic-arithmetic! g numeric-arithmetic)
+;;            ;(extend-generic-arithmetic! g function-extender)
+;;            (add-to-generic-arithmetic! g (make-symbolic-arithmetic numeric-arithmetic))
+;;    g))
+
+;(install-arithmetic! trie-full-generic-arithmetic-vector)
+
+;(((* 3
+;    (lambda (x) (lambda (y) (+ x y))) 
+;    (lambda (x) (lambda (y) (vector y x))))
+;  'a) 
+;4)
 
