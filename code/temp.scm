30ab06ee2f07933879868cd19bf0c419d9ebd2c8
Abigail D Choe <adchoe@mit.edu>
2017-05-03 20:22:21 -0400

    Added magnetic force and thing type magnet

--- code/temp.scm
+++ code/temp.scm
@@ -2,13 +2,20 @@
 
 (load "load")
 
-(define (reset-repl)
-  (ge (make-top-level-environment)))
+;(define (reset-repl)
+;  (ge (make-top-level-environment)))
 
 ;(install-arithmetic! (extend-arithmetic vector-extender numeric-arithmetic))
 
+(define pi (* 4 (atan 1 1)))
+
 (define (sum values)
   (reduce-left + 0 values))
+
+(define (sign n)
+  (cond ((negative? n) -1)
+        ((positive? n)  1)
+        (else 0)))
 
 ;;; object type (parent type of everything)
 
@@ -107,8 +114,10 @@
    'position position
    'velocity velocity))
 
-(define (add-ball! ball world)
-  (set-world-all-things! world (cons ball (get-world-all-things world)))
+; add any thing with mass (is affected by gravity)
+
+(define (add-mass! mass world)
+  (set-world-all-things! world (cons mass (get-world-all-things world)))
   (for-each (lambda (thing)
               (let* ((no-gravity
                       (remove (lambda (interaction)
@@ -119,6 +128,49 @@
                       (cons gravity no-gravity)))
                 (set-interactions! thing new-interactions)))
             (get-world-all-things world)))
+
+
+
+;;; magnet type (for point-like magnets)
+
+(define magnet:charge
+  (make-property 'charge))
+
+(define magnet?
+  (make-type 'magnet (list magnet:charge)))
+(set-predicate<=! magnet? thing?)
+
+(define get-magnet-charge
+  (property-getter magnet:charge magnet?))
+(define set-magnet-charge!
+  (property-setter magnet:charge magnet? any-object?))
+
+(define (make-magnet name charge mass position #!optional velocity)
+  ((type-instantiator magnet?)
+   'name name
+   'radius 0.01      ; predefined small radius (for drawing) since
+		     ; assuming point-like magnets; no getter/setter
+		     ; for magnet radius
+   'charge charge
+   'mass mass
+   'position position
+   'velocity velocity))
+
+(define (add-magnet! magnet world)
+  (add-mass! magnet world) ; adds gravity
+  ;(set-world-all-things! world (cons magnet (get-world-all-things world)))
+  (set-world-all-magnets! world (cons magnet (get-world-all-magnets world)))
+  (for-each (lambda (magnet)
+              (let* ((no-magnetic
+                      (remove (lambda (interaction)
+                                (eq? (get-name interaction) 'magnetic-force))
+                              (get-interactions magnet)))
+                     (magnetic (make-magnetic-force magnet (get-world-all-magnets world)))
+                     (new-interactions
+                      (cons magnetic no-magnetic)))
+                (set-interactions! magnet new-interactions)))
+            (get-world-all-magnets world)))
+
 
 ;;; interaction type
 
@@ -172,10 +224,42 @@
   (let ((influences (delq thing all-things)))
     (make-interaction gravity? 'gravity procedure influences)))
 
+
+;;; magnetic-force type (for two point-like magnets)
+
+(define magnetic-force?
+  (make-type 'magnetic-force '()))
+(set-predicate<=! magnetic-force? interaction?)
+
+(define (make-magnetic-force magnet all-magnets)
+  (define (procedure magnet influences)
+    (sum (map (lambda (influence)
+                (let* ((q1 (get-magnet-charge magnet))
+                      (q2 (get-magnet-charge influence))
+                      (mu 1.256e-6)  ; permeability of air
+                      (v (- (get-position influence)  ; vector between influence and magnet
+                                       (get-position magnet)))
+                      (r (magnitude v)) ; distance between influence and magnet
+		      (sgn (cond ((eq? (+ (sign q1) (sign q2)) 0) -1) ; if same charge, -1 (repel);
+				  (else 1))) ; opposite charge, 1 (attract)
+                      (u (* sgn (/ v r))) ; unit vector
+                      (mmag (* (* mu q1 q2)  ; magnitude of magnetic force
+                              (/ 1 (* 4 pi (square r)))))
+                    )
+                  (* u mmag)  ; multiply unit vector by magnitude of force
+                ))
+              influences)))
+  (let ((influences (delq magnet all-magnets)))
+    (make-interaction magnetic-force? 'magnetic-force procedure influences)))
+
+
 ;;; world type
 
 (define world:all-things
   (make-property 'all-things
+                 'default-value '()))
+(define world:all-magnets
+  (make-property 'all-magnets
                  'default-value '()))
 (define world:timestep
   (make-property 'timestep
@@ -183,6 +267,7 @@
 
 (define world?
   (make-type 'world (list world:all-things
+			  world:all-magnets
                           world:timestep)))
 
 (define (make-world name)
@@ -193,6 +278,10 @@
   (property-getter world:all-things world?))
 (define set-world-all-things!
   (property-setter world:all-things world? any-object?))
+(define get-world-all-magnets
+  (property-getter world:all-magnets world?))
+(define set-world-all-magnets!
+  (property-setter world:all-magnets world? any-object?))
 (define get-world-timestep
   (property-getter world:timestep world?))
 (define set-world-timestep!
@@ -205,14 +294,28 @@
 
 
 (define w (make-world "world"))
-(define b1 (make-ball "ball1" 10 1000000000000 #(0 0)))
-(define b2 (make-ball "ball2" 10 1000000000000 #(10 10)))
-(add-ball! b1 w)
-(add-ball! b2 w)
-;(eq? b (car (get-world-all-things w)))
-(get-position b1)
-(get-position b2)
+;(define b1 (make-ball "ball1" 10 1000000000000 #(0 0)))
+;(define b2 (make-ball "ball2" 10 1000000000000 #(10 10)))
+;(add-mass! b1 w)
+;(add-mass! b2 w)
+(define m1 (make-magnet "magnet1" -2 1000000000000 #(1 1)))
+(define m2 (make-magnet "magnet2" 2 1000000000000 #(10 10)))
+(add-magnet! m1 w)
+(add-magnet! m2 w)
+
+;(get-interactions b1)
+(get-interactions m1)
+;(eq? b1 (car (get-world-all-things w)))
+
+;(get-position b1)
+;(get-position b2)
+(get-position m1)
+(get-position m2)
+
 (update-world w)
-(get-position b1)
-(get-position b2)
+
+;(get-position b1)
+;(get-position b2)
+(get-position m1)
+(get-position m2)
 
